b04ae11fc0d8a230035efb00971018da8f15c0be
Zirullo Amiri <amirymax@gmail.com>
2024-03-22 05:44:20 +0300

    getting ready

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     if not started:
         webbrowser.open('http://127.0.0.1:5000')
         started = True
-    app.run(host='127.0.0.1', port=5000)
+    app.run()
 
 
 CoUninitialize()
